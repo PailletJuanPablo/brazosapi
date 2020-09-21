b8b63d716e910c560699320afb72d2ea81b98aa2
PailletJuanPablo <pailletjuanpablo@gmail.com>
2020-09-21 17:18:49 -0300

    fix - use of dotenv to manage database connections

--- config/config.js
+++ config/config.js
@@ -2,24 +2,24 @@ require('dotenv').config()
 
 module.exports = {
     "development": {
-        "username": 'root',
-        "password": '',
-        "database": 'ong',
-        "host": '127.0.0.1',
+        "username": process.env.DB_USER,
+        "password": process.env.DB_PASSWORD,
+        "database":  process.env.DB_NAME,
+        "host": process.env.DB_HOST,
         "dialect": "mysql"
     },
     "test": {
-        "username": process.env.DB_USER | 'root',
-        "password": process.env.DB_PASSWORD | '',
-        "database": process.env.DB_NAME | 'ong',
-        "host": process.env.DB_HOST | '127.0.0.1',
+        "username": process.env.DB_USER,
+        "password": process.env.DB_PASSWORD,
+        "database": process.env.DB_NAME,
+        "host": process.env.DB_HOST,
         "dialect": "mysql"
     },
     "production": {
-        "username": process.env.DB_USER | 'root',
-        "password": process.env.DB_PASSWORD | '',
-        "database": process.env.DB_NAME | 'ong',
-        "host": process.env.DB_HOST | '127.0.0.1',
+        "username": process.env.DB_USER,
+        "password": process.env.DB_PASSWORD,
+        "database": process.env.DB_NAME,
+        "host": process.env.DB_HOST,
         "dialect": "mysql"
     }
 }
