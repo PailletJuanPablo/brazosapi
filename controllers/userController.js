cc2f36f25d545066db9e18b00277cf162fb50a7d
Manuel Clifton <manuclifton@gmail.com>
2020-09-23 11:34:36 -0300

    guardado de usuarios

---
+++ controllers/userController.js
@@ -0,0 +1,48 @@
+const db = require('../models/index');
+const { validationResult } = require('express-validator');
+const bcryptjs = require('bcryptjs');
+
+
+//Crear nuevo usuario
+exports.crateUser = async (req, res) => {
+    //revisar si hay errores
+    const errors = validationResult(req);
+
+    if(!errors.isEmpty()){ // si hay errores los muestro
+        console.log('Hay un error de validacion de campos');
+        return res.status(400).json({ errors: errors.array() })
+    }
+
+    try {     
+        const {firstName, lastName ,email, password} = req.body;
+
+        //Revisar que el usuario se unico
+        const getUser = await db.User.findOne({
+            where: {email: email}
+        });
+        console.log(getUser);
+       
+        if(getUser == null){
+
+
+            let hash = await bcryptjs.hash(`${password}`,10);
+            //console.log(hash);
+            const newUser = await db.User.create({
+                firstName: firstName,
+                lastName: lastName,
+                email: email,
+                //password: hash,
+                password: hash
+            });
+
+               res.status(200).json(newUser);
+            
+        }else{
+            res.status(404).json({ msg: 'Ya existe usuario con el mismo EMAIL'});
+        }
+        
+    } catch (error) {
+        console.log(error.message);
+        res.status(400).json(error);
+    }
+}
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
+const userController = require('../controllers/userController');
+const {check} = require('express-validator');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+/* POST create user. */
+router.post('/',
+[
+  check('firstName', 'El Nombre es obligatorio').not().isEmpty(),
+  check('lastName', 'El Apellido es obligatorio').not().isEmpty(),
+  check('email', 'Agrega un Email valido').isEmail(),
+  check('password', 'El password deve contener 6 caracteres como minimo').isLength({min:6})
+],
+ userController.crateUser
+ )
 
 module.exports = router;
