f5ce48bfdaea9d0e4c29a46cdb0bec3130a927a8
santilini <santiagolky@gmail.com>
2020-09-23 15:10:47 -0300

    Servicio de subir stream a Amazon S3

---
+++ services/awsService.js
@@ -0,0 +1,25 @@
+const AWS = require('aws-sdk')
+require('dotenv').config()
+
+const BUCKET_NAME = 'alkemy-ong'
+const s3 = new AWS.S3({
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  signatureVersion: 'v4'
+})
+
+const uploadFile = async (userId, fileName, fileStream) => {
+  const params = {
+    Bucket: BUCKET_NAME,
+    Key: `${userId}/${Date.now()}-${fileName}`,
+    Body: fileStream
+  }
+  try {
+    const response = await s3.upload(params).promise()
+    return response
+  } catch(error) {
+    throw {message: error.message}
+  } 
+}
+
+module.exports = uploadFile
