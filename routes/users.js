5e70d63579e0b245bf9ab6d292f782960a9547dd
Natalia Maccarrone <ns.maccarrone@gmail.com>
2020-09-23 14:22:30 -0300

    Added GET /user endpoint

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
+const { getUser } = require('../controllers/getUserController');
+
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', function (req, res, next) {
+	res.send('respond with a resource');
 });
+
+/* GET authenticated user data */
+router.get('/user', getUser);
 
 module.exports = router;
