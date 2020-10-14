1b27e1d22a148e52ae3dddc4478c4217c950be01
damianArgote <argotedamian@gmail.com>
2020-10-14 13:28:44 -0300

    Fixed description text

--- seeders/20200922101419-create-demo-organization.js
+++ seeders/20200922101419-create-demo-organization.js
@@ -7,8 +7,6 @@ module.exports = {
       name: 'Brazos Abiertos',
       image: 'https://drive.google.com/uc?export=view&id=1SO5FoXAdaeftX701CiKp6DRr7jdgMf3C',
       description: `Brazos Abiertos
-      Descripción de organización de prueba 1
-      
       Brindamos asistencia y acompañamiento, tanto material como psicológica, a familias de bajos recursos económicos, de estratos sociales vulnerables y en las que un integrante menor de 15 años está atravesando un tratamiento médico de alguna enfermedad terminal, poco frecuente o grave.
       
       La asistencia es de forma parcial o total en el tiempo que se encuentren en tratamiento médico. Se realiza un acuerdo con la familia (dependiendo el caso, complejidad de la enfermedad, situación económica, entre otros) y en el transcurso de ese tiempo acordado los acompañamos: psicológicamente, con visitas continuas brindando acompañamiento en el hospital y en sus hogares.
