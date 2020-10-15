3a4a95f45e0b84a593ec5da5e09bc2ff72f23168
damianArgote <argotedamian@gmail.com>
2020-10-15 12:05:16 -0300

    Feature - eventsRouter

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ const organizationRouter = require('./routes/organizations');
 const newsRouter = require('./routes/news');
 const contactsRouter = require('./routes/contacts');
 const contributorsRouter = require('./routes/contributors');
+const eventsRouter = require('./routes/events');
 
 const entriesRouter = require('./routes/news')
 const emailRouter = require('./routes/email')
@@ -36,6 +37,7 @@ app.use('/entries/', entriesRouter)
 app.use('/contacts', contactsRouter )
 app.use('/emails/', emailRouter)
 app.use('/contributors', contributorsRouter);
+app.use('/events',eventsRouter);
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
