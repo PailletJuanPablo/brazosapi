a52498a0793c3557ce9895cf592f3567faaf7f5a
damianArgote <argotedamian@gmail.com>
2020-10-15 12:18:51 -0300

    Fixed - eventsController

--- controllers/eventsController.js
+++ controllers/eventsController.js
@@ -4,9 +4,9 @@ const getAll = async (req, res) => {
   try {
     const events = await eventsService.findAll();
     if (!events.length) {
-      return res.status(200).json({ message: 'No news found.', events: [] });
+      return res.status(200).json({ message: 'No events found.', events: [] });
     }
-    return res.json({ message: 'OK', news });
+    return res.json({ message: 'OK', events });
   } catch (error) {
     return res.status(500).send({ message: 'Server error' });
   }
