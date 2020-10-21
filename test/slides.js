41f952a3185e1a40dc9991498b7086c839e0df87
damianArgote <argotedamian@gmail.com>
2020-10-21 15:44:08 -0300

    Feacture - Slides Testing get,post,put

---
+++ test/slides.js
@@ -0,0 +1,99 @@
+
+const db= require('../models');
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const server = require('../app');
+const should = chai.should();
+const fs = require('fs');
+const path = require('path');
+
+
+
+chai.use(chaiHttp);
+//Our parent block
+describe('Slides', () => {
+
+  after(function(done) {
+
+    db.Slides.destroy({ truncate: { cascade: false } })
+    .then(() => {
+      res.json({ status: true });
+    }, (err) => {
+      console.log('truncate: ', err);
+      res.json(err);
+    });
+    done();
+  })
+
+
+  /*
+  * Test the /GET route
+  */
+  describe('/GET slides', () => {
+    it('it should return all slides', (done) => {
+      chai
+        .request(server)
+        .get('/slides/')
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.message.should.be.a('string');
+          res.body.message.should.be.equal('OK');
+          res.body.slides.should.be.a('array');
+          res.body.slides.length.should.be.eql(0);
+          done();
+        });
+    });
+  });
+
+  /**
+   * Test the /POST route
+   */
+
+  describe('/POST slides', () =>{
+    it('it should POST a slide', done =>{
+      let slide ={
+        bienvenida:'Testing slide',
+        text:'text testing',
+        order:6
+      }
+
+      chai
+        .request(server)
+        .post('/slides')
+        .attach('media',fs.readFileSync(path.join(__dirname,'aguila.jpg')),'aguila.jpg')
+        .field('bienvenida', 'texto de prueba')
+        .field('text', 'customValue')
+        .field('order', 7)
+        .end((err,res) =>{
+          res.should.have.status(201);
+          res.body.should.be.a('object');
+          res.body.should.have.property('bienvenida');
+          res.body.should.have.property('text');
+          res.body.should.have.property('order');
+          res.body.should.have.property('image');
+          done();
+        })
+    })
+  })
+
+  describe('/PUT/:id Slides', () => {
+    it('it should not update the slide info', (done) => {
+        const slide = {
+            bienvenida: 'Testing slide',
+            text: 'some text for testing',
+        }
+        const slideId = 78;
+         chai.request(server)
+         .put('/slides/'+ slideId)
+         .send(slide)
+         .end((err, res) => {
+             res.should.have.status(404);
+             res.body.should.be.a('object');
+             res.body.should.have.property('message');
+             res.body.message.should.be.equal('No existe un slide con ese ID');
+             done();
+         });
+    });
+  });
+
+})
