5bd87f13f27256d57faf9c92dabf0e5873c7bdae
karibelbt <karibel.barco@gmail.com>
2020-10-21 10:27:46 -0300

    feature - test get all success and error

--- test/testimonials.js
+++ test/testimonials.js
@@ -1,47 +1,51 @@
-//During the test the env variable is set to test
-
-
-//Require the dev-dependencies
-let chai = require('chai');
-let chaiHttp = require('chai-http');
-let server = require('../app');
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const server = require('../app');
 let should = chai.should();
 
-
 chai.use(chaiHttp);
-//Our parent block
+
 describe('Testimonials', () => {
 
-/*
-  * Test the /GET route
-  */
   describe('/GET testimonials', () => {
 
-      it('it should return all testimonials', (done) => {
-        chai.request(server)
-            .get('/testimonials')
-            .end((err, res) => {
-                  res.should.have.status(200);
-                  res.body.message.should.be.a('string');
-                  res.body.message.should.be.equal('No testimony found.');
-                  res.body.testimony.should.be.a('array');
-                  res.body.testimony.length.should.be.eql(0);
-              done();
-            });
-      });
+    it('it should return all testimonials', (done) => {
+      chai.request(server)
+        .get('/testimonials')
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.message.should.be.a('string');
+          res.body.message.should.be.equal('OK');
+          res.body.testimony.should.be.a('array');
+          res.body.testimony.length.should.be.eql(3);
+          done();
+        });
+    });
+    it('it should return error No Testimonies Found', (done) => {
+      chai.request(server)
+        .get('/testimonials')
+        .end((err, res) => {
+          res.should.have.status(404);
+          res.body.message.should.be.a('string');
+          res.body.message.should.be.equal('No Testimonies Found');
+          res.body.testimony.should.be.a('array');
+          res.body.testimony.length.should.be.eql(0);
+          done();
+        });
+    });
 
-      it('it should order testimonials by date if query param ?order=date is sended', (done) => {
-        chai.request(server)
-            .get('/testimonials?order=date')
-            .end((err, res) => {
-                  res.should.have.status(200);
-                  res.body.message.should.be.a('string');
-                  res.body.message.should.be.equal('No testimony found.');
-                  res.body.testimony.should.be.a('array');
-                  res.body.testimony.length.should.be.eql(0);
-              done();
-            });
-      });
+    /* it('it should order testimonials by date if query param ?order=date is sended', (done) => {
+      chai.request(server)
+          .get('/testimonials?order=date')
+          .end((err, res) => {
+                res.should.have.status(200);
+                res.body.message.should.be.a('string');
+                res.body.message.should.be.equal('No testimony found.');
+                res.body.testimony.should.be.a('array');
+                res.body.testimony.length.should.be.eql(0);
+            done();
+          });
+    }); */
 
   });
 
