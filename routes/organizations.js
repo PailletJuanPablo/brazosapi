836cf7ea44be34855816ac4a91f0df56583935a5
karibelbt <karibel.barco@gmail.com>
2020-10-23 11:49:15 -0300

    fix bugs

--- routes/organizations.js
+++ routes/organizations.js
@@ -1,8 +1,9 @@
 const router = require('express').Router()
 const organizationController = require('../controllers/organizationController')
+const requireLogin = require('../middlewares/requirelogin');
 
 router.get('/public/', organizationController.public);
 
-router.patch('/:id', organizationController.updateById);
+router.patch('/:id', requireLogin, organizationController.updateById);
 
 module.exports = router;
