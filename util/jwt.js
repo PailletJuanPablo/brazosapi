dda4adc27a88e7cc8a97a3a8c6f277ee775cbf9c
Natalia Maccarrone <ns.maccarrone@gmail.com>
2020-10-23 16:47:00 -0300

    changed jwt expiration

--- util/jwt.js
+++ util/jwt.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken')
 require('dotenv').config()
 
 const SECRET =  process.env.LOGIN_SECRET
-const EXPIRATION = '1h'
+const EXPIRATION = "48h";
 
 const generateJWT = (payload) => {
   return jwt.sign(payload, SECRET,{ algorithm: 'HS256', expiresIn: EXPIRATION })
