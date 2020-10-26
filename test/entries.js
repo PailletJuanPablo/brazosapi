d9624e90204d2d4e41e5659baafebd4bde6fc86f
Manuel Clifton <manuclifton@gmail.com>
2020-10-26 10:46:13 -0300

    feature entries test

--- test/entries.js
+++ test/entries.js
@@ -12,3 +12,26 @@ const userCredentials = {
   email: 'example@example.com',
   password: '123456789'
 }
+
+describe('Testimonials', () => {
+
+    //POST
+    it('it should return contribution object', (done) => {
+        chai.request(server).post('/contributors').send(contributionCreate)
+        .end((err, res) => {
+          res.should.have.status(201);
+          res.body.message.should.be.a('string');
+          res.body.message.should.be.equal('OK');
+          res.body.contribution.should.be.a('object');
+          res.body.contribution.should.have.property('id');
+          res.body.contribution.should.have.property('fullName');
+          res.body.contribution.should.have.property('email');
+          res.body.contribution.should.have.property('type');
+          res.body.contribution.should.have.property('message');
+          res.body.contribution.should.have.property('createdAt');
+          done();
+        });
+      });
+
+
+});
